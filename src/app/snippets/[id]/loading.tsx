6d28f1f4e1cdf7dc51d6d7783c834cf26f66f79f
Ajit_Patil <ajitp.devops@outlook.com>
2024-02-18 12:46:20 +0530

    cleaned up the new project

--- src/app/snippets/[id]/loading.tsx
+++
@@ -1,5 +0,0 @@
-const SnippetLoadingPage = () => {
-  return <div className="text-white text-xl">Loading.....</div>;
-};
-
-export default SnippetLoadingPage;
--- src/app/snippets/[id]/not-found.tsx
+++
@@ -1,9 +0,0 @@
-const NotFound = () => {
-  return (
-    <div className="text-white text-xl">
-      Sorry, could not found the snippet.
-    </div>
-  );
-};
-
-export default NotFound;
